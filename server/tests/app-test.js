2287bc4ca25c85cafe525c1dccc530d44a493510
Agnaldo Silva Lima <a.agnaldosilva@gmail.com>
2025-12-08 01:32:35 -0300

    Update app-test.js

--- server/tests/app-test.js
+++ server/tests/app-test.js
@@ -1,5 +1,14 @@
-// In server/package.json
-"scripts": {
-  // Use <rootDir>/../ to look one level up
-  "test": "jest --testMatch '<rootDir>/../tests/**/*.js'"
-}
+const request = require('supertest');
+const app = require('../app'); // Import Express app
+
+describe('Security Headers Check', () => {
+  it('should return 200 OK for root route', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it('should have X-Content-Type-Options header', async () => {
+    const res = await request(app).get('/');
+    expect(res.headers['x-content-type-options']).toEqual('nosniff');
+  });
+});
